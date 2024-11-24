610b4dae2fda683f28ca3cf0885ef70861ff3f41
Napoleon Torres <159686382+NapsTorres@users.noreply.github.com>
2024-11-24 17:03:15 +0800

    trying for hosting

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,7 @@ const { userRouter, teamsRouter, eventsRouter, sportsRouter, gameRouter, mediaRo
 
 const corsOptions = {
     origin: ['http://localhost:5173','http://localhost:5174','http://localhost:5175'], 
-    credentials: false,
+    credentials: true,
     optionsSuccessStatus: 200,
   };
   
