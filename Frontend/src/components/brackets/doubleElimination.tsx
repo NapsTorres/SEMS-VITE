178873b7265d776ec8b747b5b25b2ce8ac352327
Napoleon Torres <159686382+NapsTorres@users.noreply.github.com>
2024-12-01 11:39:47 +0800

    Adding the Final Rematch

--- Frontend/src/components/brackets/doubleElimination.tsx
+++ Frontend/src/components/brackets/doubleElimination.tsx
@@ -11,24 +11,24 @@ const DoubleEliminationBracket: React.FC<DoubleEliminationHooksProps> = ({
     championTeam,
     winnersRounds,
     losersRounds,
-    finalMatchBrackets,
+    finalRound,
+    finalRematchRound,
     findTeamById,
     handleScheduleClick,
   } = useDoubleEliminationHooks({ matches, teams });
 
   return (
-    <div className="space-y-12">
-      <h1 className="text-3xl font-bold text-center">
-        Double Elimination Bracket
-      </h1>
+    <div className="space-y-6">
 
       {/* Winners Bracket */}
       <div className="winners-bracket-section">
-        <h2 className="text-2xl font-bold text-center mb-6">Winners Bracket</h2>
-        <div className="bracket-grid grid grid-cols-4 gap-8">
-          {Object.keys(winnersRounds).map((round) => (
+        <h2 className="text-xl font-bold text-center mb-4">Winners Bracket</h2>
+        <div className="bracket-grid-winners grid grid-cols-3 gap-8">
+          {Object.keys(winnersRounds).map((round) => {
+            console.log(round)
+            return(
             <div key={round} className="round-column flex flex-col space-y-6">
-              <h3 className="round-title text-xl font-semibold text-center">
+              <h3 className="round-title text-lg font-semibold text-center">
                 Round {round}
               </h3>
               {winnersRounds[Number(round)].map((match) => (
@@ -40,17 +40,17 @@ const DoubleEliminationBracket: React.FC<DoubleEliminationHooksProps> = ({
                 />
               ))}
             </div>
-          ))}
+          )})}
         </div>
       </div>
 
       {/* Losers Bracket */}
       <div className="losers-bracket-section mt-12">
-        <h2 className="text-2xl font-bold text-center mb-6">Losers Bracket</h2>
-        <div className="bracket-grid grid grid-cols-4 gap-8">
+        <h2 className="text-xl font-bold text-center mb-4">Losers Bracket</h2>
+        <div className="bracket-grid-losers grid grid-cols-4 gap-8">
           {Object.keys(losersRounds).map((round) => (
             <div key={round} className="round-column flex flex-col space-y-6">
-              <h3 className="round-title text-xl font-semibold text-center">
+              <h3 className="round-title text-lg font-semibold text-center">
                 Round {round}
               </h3>
               {losersRounds[Number(round)].map((match) => (
@@ -65,18 +65,48 @@ const DoubleEliminationBracket: React.FC<DoubleEliminationHooksProps> = ({
           ))}
         </div>
       </div>
-
-      {/* Final Match */}
-      {finalMatchBrackets && (
-        <div className="final-match-section mt-12 text-center">
-          <h2 className="text-2xl font-bold mb-4">Final Match</h2>
-          <MatchComponent
-            match={finalMatchBrackets}
-            findTeamById={findTeamById}
-            onScheduleClick={handleScheduleClick}
-          />
+      {/* Final Bracket */}
+      <div className="losers-bracket-section mt-12">
+        <h2 className="text-xl font-bold text-center mb-4">Final</h2>
+        <div className="bracket-grid-losers grid grid-cols-4 gap-8">
+          {Object.keys(finalRound).map((round) => (
+            <div key={round} className="round-column flex flex-col space-y-6">
+              <h3 className="round-title text-lg font-semibold text-center">
+                Round {round}
+              </h3>
+              {finalRound[Number(round)].map((match) => (
+                <MatchComponent
+                  key={match.matchId}
+                  match={match}
+                  findTeamById={findTeamById}
+                  onScheduleClick={handleScheduleClick}
+                />
+              ))}
+            </div>
+          ))}
         </div>
-      )}
+      </div>
+      {/*Rematch Final Bracket */}
+      <div className="losers-bracket-section mt-12">
+        <h2 className="text-xl font-bold text-center mb-4">Final Rematch</h2>
+        <div className="bracket-grid-losers grid grid-cols-4 gap-8">
+          {Object.keys(finalRematchRound).map((round) => (
+            <div key={round} className="round-column flex flex-col space-y-6">
+              <h3 className="round-title text-lg font-semibold text-center">
+                Round {round}
+              </h3>
+              {finalRematchRound[Number(round)].map((match) => (
+                <MatchComponent
+                  key={match.matchId}
+                  match={match}
+                  findTeamById={findTeamById}
+                  onScheduleClick={handleScheduleClick}
+                />
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
 
       {/* Champion */}
       {championTeam && (
@@ -99,6 +129,7 @@ const DoubleEliminationBracket: React.FC<DoubleEliminationHooksProps> = ({
           </div>
         </div>
       )}
+
     </div>
   );
 };
