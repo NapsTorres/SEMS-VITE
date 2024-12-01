2161fbfc86f1ee76829f9ae0da1eaa245016007f
Napoleon Torres <159686382+NapsTorres@users.noreply.github.com>
2024-12-01 12:59:20 +0800

    change of testing database

--- Backend/middleware/db.js
+++ Backend/middleware/db.js
@@ -3,7 +3,7 @@ const pool = createPool({
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'sems-testing',
+  database: 'sems-testing1',
   port: 3306,
   connectTimeout: 120000  // 120 seconds
 });
