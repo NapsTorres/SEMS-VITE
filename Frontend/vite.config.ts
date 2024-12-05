9a5edda5cf130f3b1645e542b96c25f70dabb9e6
Napoleon Torres <159686382+NapsTorres@users.noreply.github.com>
2024-12-05 13:09:58 +0800

    reducing size

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import viteCompression from 'vite-plugin-compression';
+import viteImagemin from 'vite-plugin-imagemin'; // Import the plugin
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    viteCompression(), // Compress assets
+    viteImagemin() // Optimize images
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.split('node_modules/')[1].split('/')[0]; // Split node_modules
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000, // Increase the chunk size limit
+    minify: 'esbuild', // Fast minification with esbuild
+  }
+});
