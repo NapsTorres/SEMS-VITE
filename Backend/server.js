7123a31889426795a4f44c72e95ca79400bd2048
Napoleon Torres <159686382+NapsTorres@users.noreply.github.com>
2024-12-05 08:01:04 +0800

    adding deployed frontend

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,7 @@ var bodyparser = require("body-parser");
 const { userRouter, teamsRouter, eventsRouter, sportsRouter, gameRouter, mediaRouter } = require("./router/main.router.js");
 
 const corsOptions = {
-    origin: ['http://localhost:5173','http://localhost:5174','http://localhost:5175'], 
+    origin: ['http://localhost:5173','http://localhost:5174','http://localhost:5175','https://sems--pi.vercel.app/'], 
     credentials: true,
     optionsSuccessStatus: 200,
   };
