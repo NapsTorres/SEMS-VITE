ac56caa734bf6b61dd50b2607277109ec694f776
Napoleon Torres <159686382+NapsTorres@users.noreply.github.com>
2024-12-08 17:19:16 +0800

    try connecting to vercel

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,7 @@ var bodyparser = require("body-parser");
 const { userRouter, teamsRouter, eventsRouter, sportsRouter, gameRouter, mediaRouter } = require("./router/main.router.js");
 
 const corsOptions = {
-    origin: ['http://localhost:5173','http://localhost:5174','http://localhost:5175','https://sems--pi.vercel.app/'], 
+    origin: ['http://localhost:5173','http://localhost:5174','http://localhost:5175','https://ncf-sems.vercel.app'], 
     credentials: true,
     optionsSuccessStatus: 200,
   };
