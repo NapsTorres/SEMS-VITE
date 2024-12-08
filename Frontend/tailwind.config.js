b604b7307ef32213c7e8876afa11c85c76eacca2
Napoleon Torres <159686382+NapsTorres@users.noreply.github.com>
2024-12-08 13:25:47 +0800

    update

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     './index.html',
-    './src/**/*.{js,ts,jsx,tsx}', // This should be sufficient and well-scoped
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       colors: {
-        customBlack: 'rgb(17,10,0)', // For rgba(17,10,0,1)
-        customGray: 'rgb(131,127,118)', // For rgba(131,127,118,1)
-        customWhite: 'rgb(227,222,213)', // For rgba(227,222,213,1)
+        customBlack: 'rgb(17,10,0)',
+        customGray: 'rgb(131,127,118)',
+        customWhite: 'rgb(227,222,213)',
       },
     },
   },
