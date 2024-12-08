dfc050c4f2f36103c6948e0b2c4f657674d1e38c
Napoleon Torres <159686382+NapsTorres@users.noreply.github.com>
2024-12-08 12:43:04 +0800

    Resolving merge

--- Frontend/src/config/axios/index.ts
+++ Frontend/src/config/axios/index.ts
@@ -1,11 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-<<<<<<< HEAD
-  baseURL: 'http://localhost:3006/api',
-=======
   baseURL: 'https://ncf-sems.onrender.com/api',  // Make sure this matches your API URL
->>>>>>> c704a0c906f2cfecdfc97607733d1b86631bd983
   headers: {
     'X-Requested-With': 'XMLHttpRequest',  // For CORS requests
     'Content-Type': 'multipart/form-data',
